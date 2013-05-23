49d94b4d9ed7da7e5a3d32cee6361742cd09453f
johnko <git@johnko.ca>
2013-05-22 22:05:11 -0400

    don't append to tmp

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,7 +3,7 @@ cd "$(dirname "${BASH_SOURCE}")"
 git pull origin master
 function doIt() {
   rsync --exclude ".git/" --exclude ".DS_Store" --exclude "hosts" --exclude "bootstrap.sh" --exclude "README.md" -av --no-perms . ~
-  cat hosts /etc/hosts | sort -u >> /tmp/hosts ; sudo cp /tmp/hosts /etc/hosts
+  cat hosts /etc/hosts | sort -u > /tmp/hosts ; sudo cp /tmp/hosts /etc/hosts
 }
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
   doIt
