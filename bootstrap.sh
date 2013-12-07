afb545b7a36a284ef3b26989a2bc6341c6b1a4cd
johnko <git@johnko.ca>
2013-12-07 17:55:42 -0500

    ignore .osx for now

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,8 +2,15 @@
 cd "$(dirname "${BASH_SOURCE}")"
 git pull origin master
 function doIt() {
-  rsync --exclude ".git/" --exclude ".DS_Store" --exclude "hosts" --exclude "bootstrap.sh" --exclude "README.md" -virchmLP --no-perms . ~
-  cat hosts /etc/hosts | sort -u > /tmp/hosts ; sudo cp /tmp/hosts /etc/hosts
+  rsync \
+	--exclude ".git/" \
+	--exclude ".DS_Store" \
+	--exclude "hosts" \
+	--exclude "bootstrap.sh" \
+	--exclude "README.md" \
+	--exclude ".osx" \
+	-virchmLP --no-perms . ~
+  #cat hosts /etc/hosts | sort -u > /tmp/hosts ; sudo cp /tmp/hosts /etc/hosts
 }
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
   doIt
